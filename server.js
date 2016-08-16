54fc3e148b4619a9f48163180aa9b78673777ae5
msarukhanov <msarukhanov@gmail.com>
2016-08-16 18:52:54 +0500

    xcvxcv

--- server.js
+++ server.js
@@ -39,15 +39,6 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(function(req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(['https://', req.get('Host'), req.url].join(''));
-        }
-        return next();
-    });
-}
-
 app.use('/favicon.ico', express.static('./files/img/favicon.ico'));
 
 app.set('json spaces', 2);
