446790a525c6778a69c327283dc8123b297bbb93
msarukhanov <msarukhanov@gmail.com>
2016-09-02 12:55:00 +0500

    fgdfg

--- server.js
+++ server.js
@@ -16,20 +16,6 @@ uaParser = require('ua-parser');
 
 var port = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || parseInt(process.env.PORT) ||  901;
 
-var dbURI = 'mongodb://huaweiarmenia:huawei901@ds019970.mlab.com:19970/heroku_c5wd0q4r';
-
-mongoose.connect(dbURI);
-
-mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + dbURI);
-});
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose default connection error: ' + err);
-});
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
-});
-
 app.use(compression());
 app.use(express.static('' + __dirname + '/files'));
 app.set('views',[''+__dirname + '/files/templates', ''+__dirname + '/files/templates/botView/']);
